c75c86c0ab4a320dcae9e8a39142bb3eb76eb102
David Orson <onlyorson@outlook.com>
2020-07-29 18:04:00 +0100

    Contact

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from "react";
+import cv from "../Assets/CV.png";
+
+const Contact = () => {
+  return (
+    <div className="contact">
+      <img src={cv} />
+      <h1>07929 343576</h1>
+      <h1>
+        <span>onlyorson@outlook.com</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Contact;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
+import Contact from "../components/Contact";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <Projects />
+      <Contact />
     </div>
   );
 };
