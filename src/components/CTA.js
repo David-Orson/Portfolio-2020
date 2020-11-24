ea5f46a93e14cd88bf3fdde88b09bfd01f0ffe14
David Orson <onlyorson@outlook.com>
2020-11-24 22:43:16 +0000

    CTA Design and copy

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -6,14 +6,24 @@ const CTA = () => {
   return (
     <div className="cta">
       <div className="cta-main">
-        <div className="container">
-          <h1 className="projects-title">Ready?</h1>
-          <p className="projects-tagline">
+        {/* <div className="container"> */}
+          <h1 className="cta-title">Why Choose me?</h1>
+          <p className="cta-tagline-left">
             <span>
-              Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem
+              I am very excited to break into the tech world and prove what I can do! I have a history of working hard and satisfying customer and client needs. I pay close attention to communication and the needs of a client in order to deliver the most appropriate product and value for their expectations.
             </span>
           </p>
-        </div>
+          <p className="cta-tagline-right">
+            <span>
+              I have developed competency in multiple leading web development technologies and am capable of using my Firebase - Express - React - Node stack to create unique solutions to application needs. I have spent time refining my skills and studying the more nuanced distinctions of these technologies so that I am capable of delivering to your needs.
+            </span>
+          </p>
+          <p className="cta-tagline-left">
+            <span>
+              I am looking for Coding work, both contractual and individual projects for Front-end, Full-Stack and Design projects. I am available for work now and am excited to hear from you! 
+            </span>
+          </p>
+        {/* </div> */}
       </div>
     </div>
   )
