7101292ec16f01825f5bf0bbb2cd263f7343321e
David Orson <onlyorson@outlook.com>
2020-11-24 20:31:50 +0000

    Skills Design

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ const About = () => {
             <img src={profile_pic} className="pfp" />
           </div>
           <div className="about-info-container">
-            <p>Hi, my name's David and I live near Sheffield UK and am currently building web applications using Javascript and the React library. I am a self taught developer and have been pursuing coding for almost a year. Have a look at my work and get in contact if you would like to do some work together!
+            <p>Hi, my name's David and I live near Sheffield UK. I am currently building web applications using Javascript and the React library. I am a self taught developer and have been pursuing coding for almost a year. Have a look at my work and get in contact if you would like to create something our clients will love!
             {/* <h1>
               <span>Hi, I'm</span> David Orson
             </h1>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,22 +2,29 @@ import React from 'react';
 
 import "../css/Skills.css"
 
+import coding from '../Assets/coding.svg';
+import design from '../Assets/design.svg';
+import problemsolving from '../Assets/problemsolving.svg'
+
 const Skills = () => {
   return <div className="skills">
     <div className="skills-main">
-      <h1>What I Do</h1>
+      <h1 className="skills-title">What I Do</h1>
       <div className="skills-container">
       <div className="skill-container">
-        <h1>Skill</h1>
-        <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem LoremLorem Lorem Lorem Lorem</p>
+        <img src={coding} className="skill-icon" />
+        <h1>Development</h1>
+        <p>I have a strong focus and passion for web development. I understand the importance of writing clean, scalable, maintainable code which grows into elegant software. I strive to sharpen my skills and implement more technologies to bring the best user experience possible.</p>
       </div>
       <div className="skill-container">
-        <h1>Skill</h1>
-        <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem LoremLorem Lorem Lorem Lorem</p>
+        <img src={design} className="skill-icon" />
+        <h1>Design</h1>
+        <p>As a developer, it's easy to get lost in functionality, but design is essential to the user experience of the end product. I strive to improve my eye for design and regularly try new ideas to make my products pleasing to the eye.</p>
       </div>
       <div className="skill-container">
-        <h1>Skill</h1>
-        <p>Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem Lorem LoremLorem Lorem Lorem Lorem</p>
+        <img src={problemsolving} className="skill-icon" />
+        <h1>Problem Solving</h1>
+        <p>I love that moment when I find the solution to a challenge, this work always keeps me growing and on my toes. I take my time to reflect on all issues when it is necessary to find a unique approach. I also have grown competent at identifying the specific issue and using online resources.</p>
       </div>
       </div>
       
