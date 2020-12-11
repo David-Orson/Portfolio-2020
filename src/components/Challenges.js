2de67ba5924daf72e02c695d2742ad7906c80b9e
David Orson <onlyorson@outlook.com>
2020-12-11 18:35:11 +0000

    Challenges

--- src/components/Challenges.js
+++ src/components/Challenges.js
@@ -14,6 +14,26 @@ const Challenges = () => {
             </span>
           </p>
         </div>
+        <div className="challenges-container">
+          <a className="challenge" href="https://reverse-a-string.netlify.app/" target="_blank">
+            <div className="challenge-icon1"></div>
+            <div className="challenge-title-container">
+              <h2>Reverse a String</h2>
+            </div>
+          </a>
+          <a className="challenge" href="https://palindrone.netlify.app/" target="_blank">
+            <div className="challenge-icon2"></div>
+            <div className="challenge-title-container">
+              <h2>Palindrone</h2>
+            </div>
+          </a>
+          <a className="challenge" href="https://reverse-an-integer.netlify.app/" target="_blank">
+            <div className="challenge-icon3"></div>
+            <div className="challenge-title-container">
+              <h2>Reverse an Integer</h2>
+            </div>            
+          </a>
+        </div>
       </div>  
     </div>
   )
