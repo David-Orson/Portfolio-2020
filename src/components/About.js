02363d3d3eb3c64d4105f2b3a30b2bda2b5bf160
David Orson <onlyorson@outlook.com>
2020-12-12 20:08:17 +0000

    About section responsive

--- src/components/About.js
+++ src/components/About.js
@@ -13,20 +13,9 @@ const About = () => {
             <img src={profile_pic} className="pfp" />
           </div>
           <div className="about-info-container">
-            <p>Hi, my name's David and I live near Sheffield UK. I am currently building web applications using Javascript and the React library. I am a self taught developer and have been pursuing coding for almost a year. Have a look at my work and get in contact if you would like to create something our clients will love!
-            {/* <h1>
-              <span>Hi, I'm</span> David Orson
-            </h1>
             <p>
-              <span>I'M A</span> SELF TAUGHT WEB DEVELOPER
-              <span> CURRENTLY BASED NEAR</span> SHEFFIELD, UK
-              <br /> <span>LET'S BUILD SOMETHING OUR</span> CLIENTS
-              <span> AND THEIR</span> CUSTOMERS<span> WILL </span>
-              LOVE. */}
+              Hi, my name's David and I live near Sheffield UK. I am currently building web applications using Javascript and the React library. I am a self taught developer and have been pursuing coding for almost a year. Have a look at my work and get in contact if you would like to create something our clients will love!
             </p>
-            {/* <a className="button" href="#portfolio">
-              See My Work
-            </a> */}
           </div>
         </div>
         
